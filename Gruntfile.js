52f7ccfc43f8dad8cf8c8b35ffe09198a9e2d5ac
Lucio Villa <lucio310@gmail.com>
2017-04-04 16:10:42 -0700

    added wcm task to minify html and strip uneeded code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,34 @@
 module.exports = function(grunt) {
-
-  //load tasks
-  grunt.loadTasks("./tasks");
+  
+  grunt.initConfig({
+	  processhtml: {
+	    dist: {
+	      files: {
+	        'build/wcm.html': ['build/index.html']
+	      }
+	    }
+	  },
+    htmlmin: {                                     
+      dist: {                                      
+        options: {   
+          collapseWhitespace: true
+        },
+        files: {                                  
+          'build/wcm.html': 'build/wcm.html'
+        }
+      }
+    }
+	});
 
   grunt.registerTask("content", "Load content from data files", ["state", "json", "csv", "markdown"]);
   grunt.registerTask("template", "Build HTML from content/templates", ["content", "build"]);
   grunt.registerTask("static", "Build all files", ["copy", "bundle", "less", "template"]);
+  grunt.registerTask("wcm", ["clean", "static", "processhtml", "htmlmin"]);
   grunt.registerTask("default", ["clean", "static", "connect:dev", "watch"]);
+
+  //load tasks
+  grunt.loadTasks("./tasks");
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
 };
